e0daf55f1edb0df523a9aab460ee9cec63078e14
leyvamn2 <56892926+leyvamn2@users.noreply.github.com>
2019-10-30 12:00:34 -0600

    Suma

---
+++ suma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int e1,e2,suma;
+
+printf("indtroduzca el primer número");
+scanf("%d",&e1);
+
+printf("indtroduzca el segundo número");
+scanf("%d",&e2);
+
+suma=e1+e2;
+printf("la suma es%d\n",suma );
+
+return 0;
+}
