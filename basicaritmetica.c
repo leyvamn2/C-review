e41a481056591251ec842bba9aa87498a2dbbe9e
leyvamn2 <56892926+leyvamn2@users.noreply.github.com>
2019-10-31 10:18:54 -0600

    Operaciones aritméticas básicas

---
+++ basicaritmetica.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+int e1,e2,suma,resta,mult,div;
+
+printf("indtroduzca el primer número");
+scanf("%d",&e1);
+
+printf("indtroduzca el segundo número");
+scanf("%d",&e2);
+
+suma=e1+e2;
+printf("la suma es%d\n",suma );
+
+resta=e1-e2;
+printf("la resta es%d\n",resta );
+
+mult=e1*e2;
+printf("la multiplicacion es%d\n",mult );
+
+div=e1+e2;
+printf("la division es%d\n",div );
+
+
+
+return 0;
+}
